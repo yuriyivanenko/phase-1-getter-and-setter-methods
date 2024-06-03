d23dcee46f951c07cf6cffbcd37ae71981974394
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-03 14:43:21 -0400

    All tests pass

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius = radius
+    this.pi = Math.PI
+  }
+
+  get diameter(){
+    return this.radius * 2
+  }
+
+  get circumference(){
+    return 2 * this.pi * this.radius
+  }
+
+  get area(){
+    return this.pi * (this.radius ** 2)
+  }
+
+  set diameter(value){
+    this.radius = value / 2
+  }
+
+  set circumference(value){
+    this.radius = value / (2 * this.pi)
+  }
+
+  set area(value){
+    this.radius = Math.sqrt(value / this.pi)
+  }
+}
